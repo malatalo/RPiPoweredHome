4efcf87a9e5a1cc550ddc93ea44a272641066542
Miikka Alatalo <miikka.alatalo@gmail.com>
2017-07-27 20:28:14 +0300

    comment code

--- clock.py
+++ clock.py
@@ -3,21 +3,22 @@ from samplebase import SampleBase
 import time
 import os
 import threading
-#import flaskRest
 from flask import Flask
 from flask_restful import Resource, Api
 
+# flask rest
 app = Flask(__name__)
 api = Api(app)
 
-
+# colors for the screen
 colorRed = 255
 colorGreen = 0
 colorBlue = 0
 colorBrightness = 0.10
+# while loop sleep time in seconds
 waitTime = 60
 
-
+# flask rest
 class HelloWorld(Resource):
     def get(self):
         return {'red': colorRed,
@@ -27,17 +28,19 @@ class HelloWorld(Resource):
 
 api.add_resource(HelloWorld, '/')
 
-def startTheThing():
+# starting rest
+def startRest():
     app.run(host='192.168.10.50')
 
-
-class SimpleSquare(SampleBase):
+# led screen
+class SimpleClock(SampleBase):
     def __init__(self, *args, **kwargs):
-        super(SimpleSquare, self).__init__(*args, **kwargs)
+        super(SimpleClock, self).__init__(*args, **kwargs)
 
     def run(self):
         offset_canvas = self.matrix.CreateFrameCanvas()
 
+        # define the 'font' for time
         number0 = [
             " ### ",
             "#   #",
@@ -151,6 +154,7 @@ class SimpleSquare(SampleBase):
         numbers = [number0, number1, number2, number3, number4,
                    number5, number6, number7, number8, number9,numberd]
 
+        # define 'font' for date
         numberD0 = [
             " # ",
             "# #",
@@ -216,6 +220,7 @@ class SimpleSquare(SampleBase):
                     numberD5, numberD6, numberD7, numberD8, numberD9,
                     numberDd]
 
+        # define 'font' for weekdays (Finnish)
         dayMon = [
             "#   #   ##",
             "## ##  #  #",
@@ -260,44 +265,58 @@ class SimpleSquare(SampleBase):
             "###    ## "]
         days = [daySun, dayMon, dayTue, dayWed, dayThu, dayFri, daySat]
 
+        # prints given number or text (defined above) at given X and Y coordinates
         def printNumber(number, numX, numY):
+            # calculate colors
             cRed = colorRed * colorBrightness
             cGreen = colorGreen * colorBrightness
             cBlue = colorBlue * colorBrightness
+            # store starting X coordinate
             vara = numX
+            # loop through rows in 'number'
             for row in number:
+                # loop through characters in each row
                 for char in row:
+                    # if character is '#' print that pixel with calculated colors, if not paint it black
                     if char == "#":
                         offset_canvas.SetPixel(numX, numY, cRed, cGreen, cBlue)
                     else:
                         offset_canvas.SetPixel(numX, numY, 0, 0, 0)
+                    # advance one pixel to the right after each char
                     numX += 1
+                # advance one pixel down and return to the starting X coordinate
                 numY += 1
                 numX = vara
 
+        # while loop updating the clock
         while True:
-
+            # if the .txt reads 'stop' then exit everything
             if open("/home/sortsit/git/ledClock/txtClock.txt").read() == "stop\n":
                 print("txtStop")
                 os._exit(1)
 
+            # get current hours and minutes
             hours = int(time.strftime("%H"))
             minutes = int(time.strftime("%M"))
+            # split them to individual variables (eg. 15 minutes -> 1 and 5)
             hour1 = int(hours / 10)
             hour2 = hours - (hour1 * 10)
             min1 = int(minutes / 10)
             min2 = minutes - (min1 * 10)
 
+            # print each digit at specific X and Y coordinate
             printNumber(numbers[hour1], 0, 0)
             printNumber(numbers[hour2], 7, 0)
+            # dots separating hours and minutes
             printNumber(numbers[10], 14, 0)
             printNumber(numbers[min1], 20, 0)
             printNumber(numbers[min2], 27, 0)
 
+            # get current weekday and print it
             weekDay = int(time.strftime("%w"))
-
             printNumber(days[weekDay], 0, 11)
 
+            # get current day and month and print them the same way as hours and minutes
             month = int(time.strftime("%m"))
             day = int(time.strftime("%d"))
             month1 = int(month / 10)
@@ -310,16 +329,19 @@ class SimpleSquare(SampleBase):
             printNumber(numbersD[month1], 24, 11)
             printNumber(numbersD[month2], 28, 11)
 
+            # update led screen
             offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
 
+            # wait before starting while loop again
             time.sleep(waitTime)
 
 
 # Main function
 if __name__ == "__main__":
     # Thread the Flask Restful
-    t = threading.Thread(target=startTheThing)
+    t = threading.Thread(target=startRest)
     t.start()
-    simple_square = SimpleSquare()
-    if not simple_square.process():
-        simple_square.print_help()
+    # start led screen
+    simple_clock = SimpleClock()
+    if not simple_clock.process():
+        simple_clock.print_help()
